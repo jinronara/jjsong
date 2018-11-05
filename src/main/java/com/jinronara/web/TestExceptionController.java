f80c576b1d11754fa153108451fce490626f79f8
송재진 <jjsong@whatap.io>
2018-11-05 21:52:43 +0900

    11212

--- src/main/java/com/jinronara/web/TestExceptionController.java
+++ src/main/java/com/jinronara/web/TestExceptionController.java
@@ -50,9 +50,10 @@ public class TestExceptionController {
 
 		CheckNumber cn = new CheckNumber();
 		for (int i = 0; i < 20; i++) {
+			Thread.sleep(1000);
 			cn.setNumber(i);
 			cn.checkBiz();
-			Thread.sleep(1000);
+			
 		}
 
 		return "BizExceptions Occured!";
